a3de9664d9ea429c7049388ac6be4e83574e3e4e
RealWalt <84933860+RealWalt@users.noreply.github.com>
2025-05-04 21:59:52 -0400

    Delete 08-Arrays directory

--- 08-Arrays/js/02-app.js
+++
@@ -1,9 +0,0 @@
-const numeros = [10, 20, 30, 40, 50];
-
-console.log(numeros);
-console.table(numeros);
-
-
-//Acceder al arreglo
-console.log(numeros[2]);
-console.log(numeros[0]);
--- 08-Arrays/js/04-app.js
+++
@@ -1,4 +0,0 @@
-const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
-
-meses[0] = 'Nuevo Mes';
-console.table(meses);
