c25e9cd7f2403db4ba278fdb8fb3d7f8a571abcf
RealWalt <84933860+RealWalt@users.noreply.github.com>
2025-05-04 21:59:08 -0400

    Delete 10-Estructuras directory

--- 10-Estructuras/js/01-app.js
+++
@@ -1,9 +0,0 @@
-
-const puntaje = 1000;
-
-if(puntaje == 1000) {
-    console.log('Si es igual')
-} else{
-    console.log('No es igual')
-    
-}
--- 10-Estructuras/js/09-app.js
+++
@@ -1,5 +0,0 @@
-const autenticado = false;
-const puedePagar = true;
-
-console.log( autenticado ? puedePagar ? 'Si esta autenticado y puede pagar' : 'Si autenticado pero no puede pagar' : 'No esta autenticado');
-
