b327b7d30bb7bc79fd64a117246b4edd00ef4309
RealWalt <84933860+RealWalt@users.noreply.github.com>
2025-05-04 22:00:01 -0400

    Delete 01-Introduccion directory

--- 01-Introduccion/js/app.js
+++
@@ -1,8 +0,0 @@
-
- 
- const name = prompt('Whats your name?');
- const dream = alert(`tu nombre es ${name} `)
- document.querySelector('.contenido').innerHTML = `${name} esta aprendiendo JavaScript Moderno`;
-
-
-  
